52837e30ecd51fc5b84b1ee07e5192d6339c7819
KyleAnthonyHay <haykyle917@gmail.com>
2023-10-25 15:40:30 -0400

    api call cfor names

---
+++ CountriesApp-Kyle/CountriesApp-Kyle/CountriesAPI.swift
@@ -0,0 +1,48 @@
+//
+//  CountriesAPI.swift
+//  CountriesApp-Kyle
+//
+//  Created by Kyle-Anthony Hay on 10/25/23.
+//
+
+import Foundation
+
+struct CountriesAPI{
+    
+    static let shared = CountriesAPI()
+    
+    private let urlSession: URLSession
+    
+    init(urlSession: URLSession = URLSession.shared) {
+        self.urlSession = urlSession
+    }
+    
+    //API Request
+    func allCountries() async throws -> [CountryResponse]{
+        let url = URL(string: "https://restcountries.com/v3.1/all?fields=name,flags")!// create URL
+        let urlRequest = URLRequest(url: url)// create request from URL
+        let (data, _) = try await urlSession.data(for: urlRequest) //open "URL session" with created request
+        let countries = try JSONDecoder().decode([CountryResponse].self, from: data) // Decode data retrieved
+        
+        return countries
+    }
+}
+
+
+struct CountryResponse: Codable {
+    //struct for variables
+    struct Flag: Codable {
+        let png: String
+        let svg: String
+        let alt: String
+    }
+    
+    struct Name: Codable {
+        let common: String
+        let official: String
+    }
+    
+    // actual variables
+    let name: Name
+    let flags: Flag
+}
--- CountriesApp-Kyle/CountriesApp-Kyle/CountriesViewController.swift
+++ CountriesApp-Kyle/CountriesApp-Kyle/CountriesViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class CountriesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    let mockData = ["United States", "Jamaica", "Japan", "France", "Canada"]
+    var countries = [CountryResponse]()// () - empty by default
     
     lazy var tableView: UITableView = {
         let tableView = UITableView()
@@ -29,6 +29,16 @@ class CountriesViewController: UIViewController, UITableViewDataSource, UITableV
         tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         
+        Task{
+            do {
+                let countries = try await CountriesAPI.shared.allCountries()
+                self.countries = countries
+                tableView.reloadData()
+            } catch {
+                print(error)
+            }
+        }
+        
     }
     
 
@@ -38,14 +48,15 @@ class CountriesViewController: UIViewController, UITableViewDataSource, UITableV
     // Delegate
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: "myCell")
-        cell.textLabel?.text = mockData[indexPath.row]
+        let country = countries[indexPath.row]
+        cell.textLabel?.text = country.name.common
         
         return cell
     }
 
     // Data Source
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mockData.count
+        return countries.count
     }
      
      
