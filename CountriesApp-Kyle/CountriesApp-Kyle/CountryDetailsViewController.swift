44db65304e3d27637d1edf2af645ebd1102ed0e2
KyleAnthonyHay <haykyle917@gmail.com>
2023-11-13 10:07:41 -0500

    details page cont

--- CountriesApp-Kyle/CountriesApp-Kyle/CountryDetailsViewController.swift
+++ CountriesApp-Kyle/CountriesApp-Kyle/CountryDetailsViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class CountryDetailsViewController: UIViewController {
 
     private let countryName: String
+    private var countryData: CountryResponse?
     
     lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
@@ -41,6 +42,8 @@ class CountryDetailsViewController: UIViewController {
         Task{
             do {
                 let country = try await CountriesAPI.shared.getCountry(named: countryName)
+                countryData = country
+                self.tableView.reloadData()
                 print(country)
             } catch {
                 print(error)
@@ -53,14 +56,54 @@ class CountryDetailsViewController: UIViewController {
 
 extension CountryDetailsViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        switch section{
+        case 0:
+            return 3
+        case 1:
+            return 4
+        case 2:
+            return countryData?.languages?.count ?? 0
+        default:
+            return 0
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: "defaultCell")
-        cell.textLabel?.text = countryName
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "defaultCell")
+        switch indexPath{
+        case [0,0]:
+            cell.textLabel?.text = countryName
+            cell.textLabel?.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        case [0,1]:
+            break
+        case [0,2]:
+            cell.textLabel?.text = "Official Name"
+            cell.detailTextLabel?.text = countryData?.name.official
+        case [1,0]:
+            cell.textLabel?.text = "Capital"
+            cell.detailTextLabel?.text = countryData?.capital?.first
+        case [1,1]:
+            cell.textLabel?.text = "Population"
+            cell.detailTextLabel?.text = String(countryData?.population ?? 0)
+        case [1,2]:
+            guard let currency = countryData?.currencies?.first?.value else {break}
+            cell.textLabel?.text = "Currency Name"
+            cell.detailTextLabel?.text = currency.name
+        case [1,3]:
+            guard let currency = countryData?.currencies?.first?.value else {break}
+            cell.textLabel?.text = "Currency Symbol"
+            cell.detailTextLabel?.text = currency.symbol
+        default:
+            if indexPath.section == 2 {
+                guard let languages = countryData?.languages?.map({$0.1}) else {break}
+                cell.textLabel?.text = languages[indexPath.row]
+            }
+            break
+        }
         return cell
     }
     
-    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
 }
